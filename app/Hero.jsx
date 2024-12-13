0d318b28afce614494b9327f528df665d3e374dd
0amirk <amir.kamal09@gmail.com>
2024-12-13 17:39:31 +0530

    Switched to new theme

--- app/Hero.jsx
+++ app/Hero.jsx
@@ -13,10 +13,10 @@ const Hero = () => {
   });
 
   return (
-    <main className="w-full relative z-10 mt-16  h-[90vh] md:h-[80vh] flex justify-center  text-white/95 ">
+    <main className="w-full relative z-10 mt-16 h-[90vh] flex justify-center  text-white/95 ">
       <FloatingDockDemo />
       <div className="flex justify-center w-[97%] md:w-[98%] bg-black rounded-b-lg ">
-        <Splide
+        {/* <Splide
           options={{
             perPage: 1,
             height: 800,
@@ -26,28 +26,30 @@ const Hero = () => {
           className="block w-full h-full overflow-hidden rounded-b-lg"
         >
           <SplideSlide className="h-[100%]">
-            <div className="flex !h-full bg-black rounded-b-lg hero-bg ">
-              <div className="text-center flex flex-col items-center justify-start mt-4 md:mt-12  w-full px-4 py-6 text-[#B78738] flec-col ">
-                <p className="text-[38px] sm:text-[45px] md:text-[50px] font-bold font-charm">
-                  Transform Your Life
-                </p>
-                <p className="text-[30px] sm:text-[36px] md:mt-[-10px] md:text-[40px] font-charm">
-                  By Transforming Yourself!
-                </p>
-                <p className="mt-6 text-base text-center text-white md:mt-4 md:text-lg ">
-                  Your {text} <Cursor />
-                </p>
-                <div>
-                  <p className="mt-2 text-base font-semibold text-white md:mt-2 md:text-lg">
-                    <span className="text-[#B78738] text-[25px] mr-1 font-charm">
-                      ﷻ ﷲ
-                    </span>{" "}
-                    will help you to achieve all that you desire
+            <div className="flex bg-black rounded-b-lg hero-bg ">
+              <div className="text-center flex flex-col md:items-end  justify-end items-center md:justify-end w-full  text-[#B78738]">
+                <div className="px-4 py-6 rounded-t-lg bg-black/60 h-min md:w-[50%] ">
+                  <p className="text-[38px] sm:text-[45px] md:text-[50px] font-bold font-charm">
+                    Transform Your Life
                   </p>
+                  <p className="text-[30px] sm:text-[36px] md:mt-[-10px] md:text-[40px] font-charm">
+                    By Transforming Yourself!
+                  </p>
+                  <p className="mt-6 text-base text-center text-white md:mt-4 md:text-lg ">
+                    Your {text} <Cursor />
+                  </p>
+                  <div>
+                    <p className="mt-2 text-base font-semibold text-white md:mt-2 md:text-lg">
+                      <span className="text-[#B78738] text-[25px] mr-1 font-charm">
+                        ﷻ ﷲ
+                      </span>{" "}
+                      will help you to achieve all that you desire
+                    </p>
+                    <button className="border-[#B78738] text-[#B78738] border hover:text-black hover:bg-[#B78738] hover:opacity-95 rounded-lg font-medium md:text-xl ease-in-out transition-all duration-500 hover:scale-105 px-5 py-1 mt-6">
+                      Book Now
+                    </button>
+                  </div>
                 </div>
-                <button className="border-[#B78738] text-[#B78738] border hover:text-black hover:bg-[#B78738] hover:opacity-95 rounded-lg font-medium md:text-xl ease-in-out transition-all duration-500 hover:scale-105 px-5 py-1 mt-6">
-                  Book Now
-                </button>
               </div>
             </div>
           </SplideSlide>
@@ -67,7 +69,33 @@ const Hero = () => {
               </div>
             </div>
           </SplideSlide>
-        </Splide>
+        </Splide> */}
+        <div className="flex w-full h-full overflow-hidden bg-black rounded-b-lg hero-bg ">
+          <div className="flex flex-col md:items-end  justify-end items-center w-full  text-[#B78738]">
+            <div className="px-4 py-6 sm:px-12 flex justify-center flex-col md:px-16 rounded-t-lg text-start bg-black/65  md:h-[70%] w-full md:w-[50%] ">
+              <p className="text-[38px] sm:text-[45px] md:text-[50px] font-bold font-charm">
+                Transform Your Life
+              </p>
+              <p className="text-[30px] sm:text-[36px] md:mt-[-10px] md:text-[40px] font-charm">
+                By Transforming Yourself!
+              </p>
+              <p className="mt-6 text-base text-white md:mt-4 md:text-lg ">
+                Your {text} <Cursor />
+              </p>
+              <div>
+                <p className="mt-2 text-base font-semibold text-white md:mt-2 md:text-lg">
+                  <span className="text-[#B78738] text-[25px] mr-1 font-charm">
+                    ﷻ ﷲ
+                  </span>{" "}
+                  will help you to achieve all that you desire
+                </p>
+                <button className="border-[#B78738] text-[#B78738] border hover:text-black hover:bg-[#B78738] hover:opacity-95 rounded-lg font-medium md:text-xl ease-in-out transition-all duration-500 hover:scale-105 px-5 py-1 mt-6">
+                  Book Now
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
