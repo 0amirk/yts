7a1d075b28f724633a6854d0cf36502b815918de
0amirk <amir.kamal09@gmail.com>
2024-12-26 14:54:53 +0530

    Some minor tweaks in navbar

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -56,7 +56,7 @@ export default function Navbar() {
             : "opacity-100 transform translate-y-0"
         }`}
       >
-        <div className="flex items-center font-montserrat  flex-wrap  justify-around gap-4 py-1 text-[12px] text-white bg-black">
+        <div className="flex mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex-nowrap items-center font-montserrat    justify-between gap-4 py-1 text-[12px] text-white bg-black">
           <div className="flex gap-4">
             <a
               href="mailto:hello@yourtravelshop.com"
@@ -70,7 +70,7 @@ export default function Navbar() {
               <Phone className="w-4 h-4" />
               020 8554 7070
             </a>
-            <p className="items-center hidden gap-1 sm:flex">
+            <p className="items-center hidden gap-1 md:flex">
               <MapPin className="w-3 h-3 md:w-4 md:h-4" />
               96 Thorold Road, Ilford, London, IG1 4EX
             </p>
