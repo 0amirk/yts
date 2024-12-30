b7d4b406297457c87d45c4a122d289ff437e2c6e
0amirk <amir.kamal09@gmail.com>
2024-12-30 14:33:51 +0530

    Added blog1 page

---
+++ app/blog1/Blog1.jsx
@@ -0,0 +1,245 @@
+const Blog1 = () => {
+  return (
+    <main className="px-4 py-6 mt-8 mb-10 md:mt-10">
+      <p className="font-abril text-center text-[38px] sm:text-[45px] md:text-[50px] text-[#B78738]">
+        Guide For First Time Pilgrims Part 1
+      </p>
+      <div className="flex justify-center mt-4 font-montserrat">
+        <div className="max-w-[2000px] px-6 md:px-24">
+          <p className="font-bold mtext-lg">IHRAM</p>
+          <p>
+            Before entering the sacred state of IHRAM, which is necessary before
+            beginning your UMRAH, it is mandatory to do the following:
+          </p>
+
+          <ul className="list-disc list-inside">
+            <li>
+              &nbsp;&nbsp; &nbsp;Remove unwanted hair, clip nails, trim
+              moustache etc
+            </li>
+            <li>&nbsp;&nbsp; &nbsp;Make Ghusl (if possible) or make Wudhu</li>
+            <li>
+              &nbsp;&nbsp; &nbsp;Wear Ihram prior to arriving at the Meeqat
+            </li>
+            <li>&nbsp;&nbsp; &nbsp;Pray 2 raka&#39;at (optional)</li>
+          </ul>
+
+          <p>
+            <br />
+            After doing all of the above, you should make an intention for
+            performing Umrah by reciting the following words:
+          </p>
+
+          <p>
+            &ldquo;Allah humma inni ureedul amrata fa yas-sir haa lee wa
+            taqabbal haa minni &quot;
+          </p>
+
+          <p>
+            &ldquo;O Allah, I intend to perform Umrah; make it easy for me and
+            accept from me&quot;
+          </p>
+
+          <p>
+            <br />
+            For entering the state of Ihram, recite the Talbiyah:-
+          </p>
+
+          <p>
+            &ldquo;Labbayka Allaahumma labbayk, labbayka laa shareeyka laka
+            labbayk, innal-hamda wan-ni&#39;mata laka wal-mulk, laa shareeyka
+            lak &quot;
+          </p>
+
+          <p>
+            &ldquo;Here I am, O Allaah, here I am. Here I am, you have no
+            partner, here I am.&rdquo;
+            <br />
+            &ldquo;Verily all praise and blessings, and all sovereignty are
+            Yours. You have no partner.&quot;
+          </p>
+
+          <p>
+            Then you will proceed to Makkah and head to al-Masjid al-Haram,
+            while continuously repeating the Talbiyah.
+            <br />
+            &nbsp;
+            <br />
+            <strong>Being in Debt while performing Hajj and Umrah</strong>
+            <br />
+            There are doubts in the minds of people wanting to perform the Hajj
+            and Umrah, about whether or not one should perform the pilgrimage
+            while being in debt. For answering this question, it is important to
+            understand that there are two types of debts.
+          </p>
+
+          <p>
+            <br />
+            <strong>What are Scheduled Debts?</strong>
+            <br />
+            Scheduled debts are those type of debts that are required to be paid
+            back over a fixed period of time in installments. These debts
+            typically include a house, car or appliance mortgage, and those
+            debts that are to be repaid in one installment on a fixed date.
+            Settling scheduled debts before performing Hajj or Umrah is not
+            mandatory and you can perform both pilgrimages even if you are
+            repaying, or have to repay scheduled debts.
+          </p>
+
+          <p>
+            <br />
+            <strong>What are Non-scheduled Debts</strong>
+            <br />
+            Non-scheduled debts are those type of debts where there is no
+            defined repayment schedule. These debts typically include loans
+            taken from family members, friends and professional individual
+            lenders.&nbsp;
+          </p>
+
+          <p>
+            Before embarking for Hajj or Umrah, it is mandatory that you either
+            settle the entire non-scheduled debt or take permission from the
+            person you have taken the loan. Only after your lender has given you
+            permission to perform Hajj or Umrah, you should proceed towards
+            performing the pilgrimage.
+          </p>
+
+          <p>
+            <br />
+            <strong>Allah accepts only what is pure</strong>
+            <br />
+            You should ensure that you pay for the sacred pilgrimage only from
+            &ldquo;halal&rdquo; income or that income which is lawful in the
+            eyes of Islam. You must understand that Allah is pure and will
+            accept only what is pure. It is mandatory that you perform Hajj or
+            Umrah by spending money that has been earned only through
+            &ldquo;halal&rdquo; means.
+          </p>
+
+          <p>
+            Prophet Muhammad&nbsp; mentioned the case of a man who, having
+            journeyed far, is dishevelled and dusty, and who spreads out his
+            hands to the sky saying &ldquo;O Lord! O Lord!&rdquo; while his food
+            is haram (unlawful), his drink is haram, his clothing is
+            haram,&nbsp;
+            <br />
+            and he has been nourished with haram, so how can [his supplication]
+            be answered?&quot;
+          </p>
+
+          <p>
+            <br />
+            <strong>Taking belongings for Umrah &amp; Hajj</strong>
+            <br />
+            When the faithful are about to embark on the Hajj or Umrah
+            pilgrimage, the first thing that comes to their mind concerns the
+            number of items they should take along with them on the pilgrimage.
+          </p>
+
+          <p>
+            While there is no doubt that paying special attention to packing for
+            the pilgrimage is important, you should also note that Allah Himself
+            says in the Qur&rsquo;an: &ldquo;And take provision for the
+            journey&rdquo;. This means that along with physical preparation,
+            mental and spiritual preparation is also important.
+          </p>
+
+          <p>
+            <br />
+            <strong>Seek forgiveness before your pilgrimage </strong>
+          </p>
+
+          <p>
+            When there are some days left for your departure to the holy land
+            for perform the sacred pilgrimage, you should meet your relatives
+            and friends and seek their forgiveness.
+          </p>
+
+          <p>
+            If you have difference with someone or are not on talking terms with
+            them, you should meet them and resolve your differences, rather than
+            performing the pilgrimage while keeping the grudge in your heart and
+            mind.
+          </p>
+
+          <p>
+            Keep in mind that the purpose of making this journey is to seek
+            acceptance of our deeds and our shortcomings from Allah.
+          </p>
+
+          <p>
+            It is important tat you are kind and compassionate towards your
+            family, relatives and friends, ask them for forgiveness for any of
+            your wrongdoings and also forgive them for any of their wrongdoings.
+          </p>
+
+          <p>
+            <br />
+            <strong>Preparing your body is important</strong>
+          </p>
+
+          <p>
+            Be prepared for exertion and tiredness during your pilgrimage, as it
+            will surely take a physical toll on your body. During the entire
+            pilgrimage, you will walk much more than you have walked earlier.
+          </p>
+
+          <p>
+            To give you an example, the distance of the walking ritual between
+            Safa and Marwa for commemorating the painful journey undertaken by
+            Lady Hajar to find water for her son, is more than 3 km.
+          </p>
+
+          <p>
+            We suggest you prepare for this ritual by walking briskly daily for
+            at least 5 miles in one session. This will ensure that when you have
+            to walk during Hajj and Umrah, your body will not feel too tired,
+            and you will be able to comfortably perform the rituals.
+          </p>
+
+          <p>
+            <br />
+            <strong>Preparing the mind </strong>
+          </p>
+
+          <p>
+            For truly appreciating all the sacred rituals of Hajj and Umrah, it
+            is important to keep two things in mind, Dhikr (remembrance) and
+            Taslim (complete submission to Allah).
+          </p>
+
+          <p>
+            <br />
+            <strong>DHIKR</strong>
+          </p>
+
+          <p>
+            During the entire pilgrimage, you should keep Allah in our mind and
+            keep reminding yourself of the objective of your holy journey. You
+            should do this by regularly repeating the Talbiyah &ldquo;Here I
+            come to Allah to perform Umrah or Hajj.&rdquo;
+          </p>
+
+          <p>
+            <br />
+            <strong>TASLIM</strong>
+          </p>
+
+          <p>
+            Without wondering about the purpose of each ritual of Hajj and
+            Umrah, you should completely and unconditionally surrender to the
+            will of Allah, taking inspiration of the saying of Prophet Muhammad
+            ﷺ &ldquo;Take from me your rituals&rdquo;. You should fully believe
+            in the fact that all of Islam&rsquo;s rituals come from Allah and
+            his Messenger and are filled with meaning. All we have to do is
+            simply surrender to the will and order of Allah, and humbly do what
+            He has ordained for us.
+          </p>
+
+          <p>&nbsp;</p>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default Blog1;
